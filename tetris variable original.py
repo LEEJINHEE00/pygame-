29732301374e4bcebc672634f0be8d6ba347eada
LEEJINHEE00 <74548254+LEEJINHEE00@users.noreply.github.com>
2020-12-07 16:02:41 +0900

    Add files via upload

---
+++ tetris variable original.py
@@ -0,0 +1,14 @@
+def main():
+    """ 메인 루틴 """
+    global INTERVAL
+    count = 0
+    score = 0
+    game_over = False
+    smallfont = pygame.font.SysFont(None, 36)
+    largefont = pygame.font.SysFont(None, 72)
+    message_over = largefont.render("GAME OVER!!",
+                                    True, (0, 255, 225))
+    message_rect = message_over.get_rect()
+    message_rect.center = (300, 300)
+
+    go_next_block(INTERVAL)
